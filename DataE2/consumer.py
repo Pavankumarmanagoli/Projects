5b4475f0a9a8d72f448a7dec1558c434373f3b2f
Pavankumar Umesh Managoli <151952740+Pavankumarmanagoli@users.noreply.github.com>
2024-11-27 10:08:51 +0100

    Delete DataE2 directory

--- DataE2/consumer.py
+++
@@ -1,8 +0,0 @@
-import pulsar
-
-client = pulsar.Client('pulsar://localhost:6650')
-consumer = client.subscribe('my-topic', 'my-subscription')
-msg = consumer.receive()
-print("Received message: '%s'" % msg.data().decode('utf-8'))
-consumer.acknowledge(msg)
-client.close()
